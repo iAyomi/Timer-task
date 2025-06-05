e13e4fbce42f4a19ace0247d3cbfdc6ff6656a76
iAyomi <ayomitunde.isijola@gmail.com>
2025-06-05 14:53:38 +0100

    fixes: major bug resolve

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,41 +2,47 @@ import { useState } from "react";
 
 const Timer = () => {
   const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(55);
-
-  console.log({ minutes });
+  const [seconds, setSeconds] = useState(0);
 
   const increaseMinute = () => {
-    setMinutes((prev) => {
-      if (prev == 59) return 0;
-      return prev + 1;
+    setMinutes((prevMinutes) => {
+      if (prevMinutes == 59) return 0;
+      return prevMinutes + 1;
     });
   };
 
   const decreaseMinute = () => {
-    setMinutes((prev) => {
-      if (prev == 0) return 59;
-      return prev - 1;
+    setMinutes((prevMinutes) => {
+      if (prevMinutes == 0) return 59;
+      return prevMinutes - 1;
     });
   };
 
   const increaseSecond = () => {
-    setSeconds((prev) => {
-      if (prev == 59) {
-        increaseMinute();
+    setSeconds((prevSeconds) => {
+      if (prevSeconds === 59) {
+        if (minutes === 59) {
+          setMinutes(0);
+        } else {
+          setMinutes(minutes + 1);
+        }
         return 0;
       }
-      return prev + 1;
+      return prevSeconds + 1;
     });
   };
 
   const decreaseSecond = () => {
-    setSeconds((prev) => {
-      if (prev == 0) {
-        decreaseMinute();
+    setSeconds((prevSeconds) => {
+      if (prevSeconds === 0) {
+        if (minutes === 0) {
+          setMinutes(59);
+        } else {
+          setMinutes(minutes - 1);
+        }
         return 59;
       }
-      return prev - 1;
+      return prevSeconds - 1;
     });
   };
 
@@ -44,13 +50,25 @@ const Timer = () => {
     <div>
       <div className="flex items-center justify-center gap-x-5 text-9xl">
         <div className="flex items-center justify-center gap-x-5">
-          <div className="text-6xl flex flex-col items-center gap-y-5">
-            <button onClick={increaseMinute}>🔼</button>
-            <button onClick={decreaseMinute}>🔽</button>
+          <div className="text-8xl flex flex-col items-center gap-y-5">
+            <button
+              type="button"
+              onClick={increaseMinute}
+              className="cursor-pointer"
+            >
+              🔼
+            </button>
+            <button
+              type="button"
+              onClick={decreaseMinute}
+              className="cursor-pointer"
+            >
+              🔽
+            </button>
           </div>
           <div className="flex flex-col items-center gap-y-5">
             <div className="p-5 border-3 border-gray-300 rounded-2xl">
-              {minutes}
+              {minutes == 0 ? "00" : minutes < 10 ? `0${minutes}` : minutes}
             </div>
             <p className="text-2xl font-semibold">Minutes</p>
           </div>
@@ -61,13 +79,25 @@ const Timer = () => {
         <div className="flex items-center justify-center gap-x-5">
           <div className="flex flex-col items-center gap-y-5">
             <div className="p-5 border-3 border-gray-300 rounded-2xl">
-              {seconds}
+              {seconds == 0 ? "00" : seconds < 10 ? `0${seconds}` : seconds}
             </div>
             <p className="text-2xl font-semibold">Seconds</p>
           </div>
-          <div className="text-6xl flex flex-col items-center gap-y-5">
-            <button onClick={increaseSecond}>🔼</button>
-            <button onClick={decreaseSecond}>🔽</button>
+          <div className="text-8xl flex flex-col items-center gap-y-5">
+            <button
+              type="button"
+              onClick={increaseSecond}
+              className="cursor-pointer"
+            >
+              🔼
+            </button>
+            <button
+              type="button"
+              onClick={decreaseSecond}
+              className="cursor-pointer"
+            >
+              🔽
+            </button>
           </div>
         </div>
       </div>
